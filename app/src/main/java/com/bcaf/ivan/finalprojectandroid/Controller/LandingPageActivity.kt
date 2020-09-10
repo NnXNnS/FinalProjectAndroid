9489d1dcdb0fe4f2025a7bc4a2075312f1eb8439
Rosa <rosagaghana@gmail.com>
2020-09-10 15:45:32 +0700

    Landign page layout (general layout + bus rv)

---
+++ app/src/main/java/com/bcaf/ivan/finalprojectandroid/Controller/LandingPageActivity.kt
@@ -0,0 +1,12 @@
+package com.bcaf.ivan.finalprojectandroid.Controller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bcaf.ivan.finalprojectandroid.R
+
+class LandingPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_landing_page)
+    }
+}
